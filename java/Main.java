f6aa4b8d09690ac5220b3bbb374546daba805e7b
Juan Rangel de Prada <juansolobolos@gmail.com>
2023-09-19 15:57:26 +0200

    Primera feature juan rangel

--- java/Main.java
+++ java/Main.java
@@ -1,5 +1,11 @@
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hola mundo");
+		adiosMundo();
+	}
+
+
+	public static void adiosMundo(){
+		System.out.println("Adios mundo"); 
 	}
 }
